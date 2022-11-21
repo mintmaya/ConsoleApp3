97320f6607a7cfea2eba895a37392c38a97485b8
mintmaya <118422299+mintmaya@users.noreply.github.com>
2022-11-21 17:03:45 +0900

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib;
+
+namespace ConsoleApp3
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Person func = new Person();
+            func.Hello("Irina");
+            
+            Console.WriteLine(System.Environment.CurrentDirectory);
+            Console.ReadLine();
+          
+            
+            void name()
+            {
+                Console.WriteLine("How do u doing?");
+                Console.ReadLine();
+            }
+
+            name();
+            string GetMessage()
+            {
+                return "What is ur name?";
+                
+            }
+
+            string message = GetMessage();  
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+            int Sum(int x, int y)
+            {
+                return x + y;
+            }
+
+            int result = Sum(1345, 1345);  
+            Console.WriteLine(result);   
+
+            Console.WriteLine(Sum(55678, 676));
+
+            void PrintPerson(string nam, int age)
+            {
+                if (age > 120 || age < 1)
+                {
+                    Console.WriteLine("Du hust");
+                    return;
+                }
+                Console.WriteLine($"Имя: {nam}  Возраст: {age}");
+            }
+
+            PrintPerson("Alena", 18);  
+        }
+    }
+}
---
+++ mylib/mylib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mylib
+{
+    public class Class1
+    {
+    }
+}
